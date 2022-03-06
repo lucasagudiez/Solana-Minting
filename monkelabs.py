11aa2fd3d89f8f6e664cce13e43dc3daddf653b1
SolanaNFTCollector <99330527+SolanaNFTCollector@users.noreply.github.com>
2022-03-06 22:30:12 +0100

    update

--- monkelabs.py
+++ monkelabs.py
@@ -11,6 +11,10 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+
+from webdriver_manager.chrome import ChromeDriverManager
+
+
 def mint(values, isWindows):
     
     def selectWallet():
